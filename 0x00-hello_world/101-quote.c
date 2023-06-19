ef36a936111384e30bbe04c11f5c803d918e2460
Dewralsmith <dewralsmith@gmail.com>
2023-06-19 14:52:31 -0700

    A  C program that prints exactly and that piece of art is useful

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,18 @@
 #include <unistd.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 1 (success)
  */
+
 int mail(void)
+
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return(1);
+
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+
+return (1);
+
 }
